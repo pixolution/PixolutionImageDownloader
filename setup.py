5952d0457b8630c2d72b1efd693c1918f1dc7880
Sebastian Lutter <lutter@pixolution.de>
2019-09-12 11:00:41 +0200

    set version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='PythonImageDownloader',
-      version='0.1.0',
+      version='1.0.1',
       packages=['downloader'],
       entry_points={
           'console_scripts': [
