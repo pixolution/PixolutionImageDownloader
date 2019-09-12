92d954adcf59b8652526070ce96bd5c714286234
Sebastian Lutter <lutter@pixolution.de>
2019-09-12 20:58:13 +0200

    Set version to 1.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='PythonImageDownloader',
-      version='1.0.1',
+      version='1.2.0',
       packages=['downloader'],
       entry_points={
           'console_scripts': [
