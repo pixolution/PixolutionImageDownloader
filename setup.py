8c62982b8162894a84902012355257a52bcf0073
Sebastian Lutter <lutter@pixolution.de>
2021-10-25 12:16:28 +0200

    Adding dependencies to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(name='PythonImageDownloader',
       version='1.2.0',
       packages=['downloader'],
+      install_requires=[
+          'tqdm',
+          'requests'
+      ],
       entry_points={
           'console_scripts': [
               'pxl_downloader = downloader.__main__:main'
